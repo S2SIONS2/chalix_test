e18b7bcbeeedb3b6b48f501a8c745964eec77b16
Sion <chasio100@naver.com>
2025-02-27 19:43:53 +0900

    인트로 메세지 gsap 라이브러리 적용

--- chalix/src/components/Intro.jsx
+++ chalix/src/components/Intro.jsx
@@ -1,40 +1,30 @@
 import './intro.css';
 
 import bgVideo from '../assets/mainvid.mp4';
-import { useEffect, useState } from 'react';
+
+import { gsap } from 'gsap';
+import { useEffect, useRef } from 'react';
 
 const Intro = () => {
     const text = '지속 가능한 미래와 고객의 비즈니스 성공을 위한 혁신적인 환경 솔루션을 제공합니다';
-    const [title, setTitle] = useState([]);
-    const [count, setCount] = useState(0);
-    
+    const textRef = useRef();
     useEffect(() => {
-        if (count === 0) {
-            const delayStart = setTimeout(() => setCount(1), 800);
-            return () => clearTimeout(delayStart);
-        }
+        gsap.fromTo(
+            textRef.current.querySelectorAll("span"),
+            { opacity: 0 },
+            { opacity: 1, stagger: 0.03, duration: 0.25, ease: "none", delay: 0.1, }
+        )
+    }, [])
     
-        if (count > text.length) return;
-    
-        const typing = setTimeout(() => {
-            setTitle((prev) => [...prev, text[count - 1] === ' ' ? '\u00A0' : text[count - 1]]);
-            setCount((prev) => prev + 1);
-        }, 25);
-    
-        return () => clearTimeout(typing);
-    }, [count]);
-    
-    
-
     return (
         <section className="intro">
             <div className="intro_wrap">
                 <div className="intro_text_wrap">
                     <div className="intro_text">
-                        <h1 className='intro_title'>
-                            {title.map((char, index) => (
-                                <span key={index} style={{opacity: 1}}>
-                                    {char}
+                        <h1 className='intro_title' ref={textRef}>
+                            {text.split("").map((char, index) => (
+                                <span key={index}>
+                                    {char === " " ? "\u00A0" : char}
                                 </span>
                             ))}
                         </h1>
